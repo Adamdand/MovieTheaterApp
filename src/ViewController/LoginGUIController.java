ee15d01e2b6a55bacf793d1abf8833281bc831c3
Adam D'Andrea <58803595+Adamdand@users.noreply.github.com>
2020-11-29 22:54:25 -0700

    added function

--- src/ViewController/LoginGUIController.java
+++ src/ViewController/LoginGUIController.java
@@ -2,10 +2,13 @@ package ViewController;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
 
 import javax.swing.WindowConstants;
 
 import Controller.ModelController;
+import RegisteredUserModel.RegisteredUser;
+import RegisteredUserModel.RegisteredUserList;
 import View.LoginGUI;
 import View.TheaterGUI;
 
@@ -13,6 +16,7 @@ import View.TheaterGUI;
 public class LoginGUIController implements ActionListener{
 	private LoginGUI gui;
 	private ModelController model;
+	private RegisteredUserList userList;
 	
 	//make aggregation relationship
 	public LoginGUIController(LoginGUI gui, ModelController model) {
@@ -35,9 +39,10 @@ public class LoginGUIController implements ActionListener{
 		
 		if(e.getSource() == gui.getCreateLoginBtn()) {
 			//createLogin for non reg user
-			gui.getCreateUserNameInput();
-			gui.getCreatePasswordInput();
-			gui.getCreditCardInput();
+			String userName = gui.getCreateUserNameInput().getText();
+			String password = gui.getCreatePasswordInput().getText();
+			String creditCard = gui.getCreditCardInput().getText();
+			createNewUser(userName,password,creditCard);
 		}
 		
 		if(e.getSource() == gui.getLoginBtn()) {
@@ -49,6 +54,7 @@ public class LoginGUIController implements ActionListener{
 	
 		if(e.getSource() == gui.getVoucherBtn()) {
 			//return discount voucher
+			//we either need to connect to SQL here and add it to SQL, or make a voucherList in java to add to *****
 			gui.getVoucherCodeInput();
 
 		}
@@ -66,5 +72,25 @@ public class LoginGUIController implements ActionListener{
 
 	public void setModel(ModelController model) {
 		this.model = model;
+	}
+	
+	public void createNewUser(String email, String password, String creditCard) {
+		LocalDate today = LocalDate.now();
+		
+		int year = today.getYear();
+		int month = today.getMonthValue();
+		int day = today.getDayOfMonth();
+		
+		//System.out.println(year+ "" +month+""+day);
+		String subStart = year+""+month+""+day;
+		int subStartInt = Integer.parseInt(subStart);
+		
+		int year2 = today.getYear()+1;
+		String subEnd = year2+""+month+""+day;
+		int subEndInt = Integer.parseInt(subEnd);
+
+		RegisteredUser newUser = new RegisteredUser(email, password, creditCard,subStartInt, subEndInt);
+		userList.addUser(newUser);	//not sure if I can do it this way ******
+
 	}
 }
